fc13e66980008f2c29aec15745c4b76146ea2b44
Nahuel Aparicio <nahuel.ap.code@gmail.com>
2025-09-03 11:57:34 +0200

    Initial Project

---
+++ 2D_Base/GameManager.h
@@ -0,0 +1,18 @@
+﻿#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+
+class GameManager
+{
+public:
+
+    GameManager();
+    ~GameManager();
+    void Run();
+
+private:
+};
+
+
+
+
---
+++ 2D_Base/main.cpp
@@ -0,0 +1,9 @@
+﻿
+#include "GameManager.h"
+int main()
+{
+    GameManager* game_manager = new GameManager();
+
+    game_manager->Run();
+
+}
