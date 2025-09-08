984f99ac44dc7b75929e4da8965d73b6b6cf480a
Nahuel Aparicio <nahuel.ap.code@gmail.com>
2025-09-09 00:10:37 +0200

    Starting Inputs Base

---
+++ 2D_Base/engine/core/Input.cpp
@@ -0,0 +1,50 @@
+﻿#include "Input.h"
+
+void Input::BeginFrame()
+{
+
+}
+
+void Input::Consume(const sf::Event& e)
+{
+}
+
+void Input::EndFrame()
+{
+}
+
+bool Input::GetKey(sf::Keyboard::Key k) const
+{
+}
+
+bool Input::GetKeyDown(sf::Keyboard::Key k) const
+{
+}
+
+bool Input::GetKeyUp(sf::Keyboard::Key k) const
+{
+}
+
+bool Input::GetMouseButton(sf::Mouse::Button b) const
+{
+}
+
+bool Input::GetMouseButtonDown(sf::Mouse::Button b) const
+{
+}
+
+bool Input::GetMouseButtonUp(sf::Mouse::Button b) const
+{
+}
+
+void Input::ClearAll()
+{
+}
+
+sf::Vector2i Input::GetMousePosition() const { return _mousePosition; }
+
+sf::Vector2i Input::GetMouseDelta() const { return _mouseDelta; }
+
+float Input::GetWheelDelta() const { return _wheelDelta; }
+
+const std::u32string& Input::GetTextEntered() const { return _textEntered; }
---
+++ 2D_Base/engine/core/Input.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include "SFML/Window/Event.hpp"
+
+
+class Input
+{
+public:
+
+    void BeginFrame(); // Clears Deltas (wheel, text)m snapshot prev
+    void Consume(const sf::Event& e);
+    void EndFrame(); // Swap prev/current
+
+    // Keyboard
+
+    bool GetKey(sf::Keyboard::Key k) const; // held
+    bool GetKeyDown(sf::Keyboard::Key k) const; // pressed this frame
+    bool GetKeyUp(sf::Keyboard::Key k) const; // released this frame
+
+    // Mouse
+
+    bool GetMouseButton(sf::Mouse::Button b) const;
+    bool GetMouseButtonDown(sf::Mouse::Button b) const;
+    bool GetMouseButtonUp(sf::Mouse::Button b) const;
+
+    void ClearAll();
+
+    // Map actions (Unity - style)
+
+    // Getters
+    sf::Vector2i GetMousePosition() const;
+    sf::Vector2i GetMouseDelta() const;
+    float GetWheelDelta() const;
+    const std::u32string& GetTextEntered() const;
+
+
+private:
+
+    sf::Vector2i _mousePosition {0,0};
+    sf::Vector2i _mousePrevPosition {0,0};
+    sf::Vector2i _mouseDelta {0,0};
+    float _wheelDelta = 0.0f;
+
+    std::u32string _textEntered;
+
+};
+
+
