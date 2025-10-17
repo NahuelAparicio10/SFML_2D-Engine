173c1c5ddfd39f3d95533c7fe008465d004d61fc
Nahuel Aparicio <nahuel.ap.code@gmail.com>
2025-10-17 15:24:55 +0200

    Base Game Objects & Base Components

---
+++ 2D_Base/engine/components/Component.cpp
@@ -0,0 +1,32 @@
+﻿#include "Component.h"
+
+#include "../scene/GameObject.h"
+
+void Component::SetEnabled(bool v)
+{
+    if (enabled == v) return;
+
+    enabled = v;
+
+    if (!gameObject) return;
+
+    if (gameObject->IsActiveInHierarchy())
+    {
+        if (enabled)
+        {
+            if (!enabledInvoked)
+            {
+                OnEnable();
+                enabledInvoked = true;
+            }
+        }
+        else
+        {
+            if (enabledInvoked)
+            {
+                OnDisable();
+                enabledInvoked = false;
+            }
+        }
+    }
+}
---
+++ 2D_Base/engine/components/Transform.cpp
@@ -0,0 +1,10 @@
+﻿//
+// Created by nahue on 18/09/2025.
+//
+
+#include "Transform.h"
+
+std::type_index Transform::GetType() const
+{
+    return typeid(Transform);
+}
